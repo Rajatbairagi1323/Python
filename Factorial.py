4dc87a71d351d43d9cff44c0ef396cddf7f9d2a6
Rajatbairagi1323 <114255434+Rajatbairagi1323@users.noreply.github.com>
2023-08-06 11:45:39 +0530

    Make a programme for find factorial of given number

--- Factorial.py
+++ Factorial.py
@@ -1,6 +1,8 @@
-fac = int(input("Enter your number: "))
-f=1
-for i in range (1,fac+1):
+# Write a factorial programme in python ? 
+
+fact=int(input("Enter your number: "))  # fact is a variable
+f=1       # f is a initial starting  value
+for i in range (1,fac+1):   # use of range function 
     f=f*i
 print("factorial is = ",f)
 
