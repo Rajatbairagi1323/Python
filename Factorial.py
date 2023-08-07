7aaf56669e0b52100b567040f95ef8234eb8a065
Rajatbairagi1323 <114255434+Rajatbairagi1323@users.noreply.github.com>
2023-08-07 22:59:25 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,8 +1,8 @@
 # Write a factorial programe in python ? 
 
 fact=int(input("Enter your number: "))  # fact is a variable
-f=1       # f is a initial starting  value
+initial_value=1       # f is a initial starting  value
 for i in range (1,fac+1):   # use of range function 
-    f=f*i
-print("factorial is = ",f)
+    initial_value=initial_value*i
+print("factorial is = ",initial_value)
 
